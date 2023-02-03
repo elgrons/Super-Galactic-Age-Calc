62b3f3743bfc05bba1160ca7572758ea38501936
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-03 12:52:46 -0800

    Back from lunch. WIP refactoring SolarAge class to compute the age instead of a separate function after reread of project objectives.

--- src/js/solarage.js
+++ src/js/solarage.js
@@ -7,14 +7,14 @@ export default class SolarAge {
     this.jupiterAge = jupiterAge;
   }
 
-  currentAge() {
-    let year = this.earthAge;
-    this.mercAge = year * .24;
-    this.venusAge = year * .62;
-    this.marsAge = year * 1.88;
-    this.jupiterAge = year * 11.86;
-    return ;
-  }
+  // currentAge() {
+  //   let year = this.earthAge;
+  //   this.mercAge = year * .24;
+  //   this.venusAge = year * .62;
+  //   this.marsAge = year * 1.88;
+  //   this.jupiterAge = year * 11.86;
+  //   return ;
+  // }
   
   //current age
   //years past
