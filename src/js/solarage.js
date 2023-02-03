70fec4dda4f899f34c627a10146ab2fcfc14fc0c
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-03 14:31:13 -0800

    refactor of SolarAge constructor test passes.

--- src/js/solarage.js
+++ src/js/solarage.js
@@ -1,10 +1,10 @@
 export default class SolarAge {
-  constructor(earthAge, mercAge, venusAge, marsAge, jupiterAge) {
-    // this.earthAge = earthAge;
-    // this.mercAge = this.earth age * .24;
-    // this.venusAge = thias.earth age * .62;
-    // this.marsAge = this.earth age * 1.88;
-    // this.jupiterAge = this.earth age * 11.86;
+  constructor(earthAge) {
+    this.earthAge = earthAge;
+    this.mercAge = this.earthAge * .24;
+    this.venusAge = this.earthAge * .62;
+    this.marsAge = this.earthAge * 1.88;
+    this.jupiterAge = this.earthAge * 11.86;
   }
 
   
