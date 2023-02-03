a23d3c6c5e8e9bc30eed112663e98feb77b38d04
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-03 14:27:01 -0800

    SolarAge function good fail, all objects are undefined.

--- src/js/solarage.js
+++ src/js/solarage.js
@@ -1,10 +1,10 @@
 export default class SolarAge {
   constructor(earthAge, mercAge, venusAge, marsAge, jupiterAge) {
     // this.earthAge = earthAge;
-    // this.mercAge = mercAge;
-    // this.venusAge = venusAge;
-    // this.marsAge = marsAge;
-    // this.jupiterAge = jupiterAge;
+    // this.mercAge = this.earth age * .24;
+    // this.venusAge = thias.earth age * .62;
+    // this.marsAge = this.earth age * 1.88;
+    // this.jupiterAge = this.earth age * 11.86;
   }
 
   
