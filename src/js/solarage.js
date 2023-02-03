f196ab59a03c58f73bf195969e6c090abfcc0edc
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-03 14:22:05 -0800

    WIP: redo of this main branch after working on branch firsttry.

--- src/js/solarage.js
+++ src/js/solarage.js
@@ -1,10 +1,10 @@
 export default class SolarAge {
   constructor(earthAge, mercAge, venusAge, marsAge, jupiterAge) {
-    this.earthAge = earthAge;
-    this.mercAge = mercAge;
-    this.venusAge = venusAge;
-    this.marsAge = marsAge;
-    this.jupiterAge = jupiterAge;
+    // this.earthAge = earthAge;
+    // this.mercAge = mercAge;
+    // this.venusAge = venusAge;
+    // this.marsAge = marsAge;
+    // this.jupiterAge = jupiterAge;
   }
 
   
