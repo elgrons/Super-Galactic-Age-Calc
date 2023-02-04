96c75f07e13440156124e28301e6973bcd21480d
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-03 16:37:33 -0800

    mercTime test passes.

--- src/js/futurebday.js
+++ src/js/futurebday.js
@@ -3,6 +3,7 @@ export default class FutureBday {
   constructor(earthAge, ageIHopeToLiveUntil) {
     this.earthAge = earthAge;
     this.ageIHopeToLiveUntil = ageIHopeToLiveUntil;
+    this.mercTime = (this.ageIHopeToLiveUntil - this.earthAge) / .24;
   }
 
 }
