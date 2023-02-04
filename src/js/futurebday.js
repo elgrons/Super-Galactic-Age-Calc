e6076df3560af8854964518989d96360fa2ad64b
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-03 16:47:07 -0800

    jupiterTime test passes.

--- src/js/futurebday.js
+++ src/js/futurebday.js
@@ -6,6 +6,7 @@ export default class FutureBday {
     this.mercTime = (this.ageIHopeToLiveUntil - this.earthAge) / .24;
     this.venusTime = +((this.ageIHopeToLiveUntil - this.earthAge) / .62).toFixed();
     this.marsTime = +((this.ageIHopeToLiveUntil - this.earthAge) / 1.88).toFixed();
+    this.jupiterTime = +((this.ageIHopeToLiveUntil - this.earthAge) / 11.86).toFixed();
   }
 
 }
